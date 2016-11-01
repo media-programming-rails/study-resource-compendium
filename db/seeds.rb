ab04732913a8740480770dfe0f4eebefa9d4e742
Timo N <timo.neumann@student.htw-berlin.de>
2016-11-01 21:55:37 +0100

    populated seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Category.delete_all
+Category.create(name: "Git", description: "Version control")
+Category.create(name: "Ruby", description: "Object Oriented Programming language")
+Resource.delete_all
+Resource.create(name: "GitHub", description: "How people build software", category_id: Category.where(name: "Git").first, score: 0)
+Resource.create(name: "RubyOnRails", description: "Imagine what you could build if you learned Ruby on Rails...", category_id: Category.where(name: "Ruby").first, score: 0)
+
+
